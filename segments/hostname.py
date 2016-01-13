21bd271335d12ce736beb13337cffce73edbbc44
Thomas Perret <thomas.perret@inria.fr>
2016-01-13 23:02:25 +0100

    Fix some merging mistakes

--- segments/hostname.py
+++ segments/hostname.py
@@ -11,17 +11,6 @@ def add_hostname_segment(powerline):
 
             powerline.append(host_prompt, FG, BG)
         else:
-        if powerline.args.colorize_hostname:
-            from lib.color_compliment import stringToHashToColorAndOpposite
-            from lib.colortrans import rgb2short
-            from socket import gethostname
-            hostname = gethostname()
-            FG, BG = stringToHashToColorAndOpposite(hostname)
-            FG, BG = (rgb2short(*color) for color in [FG, BG])
-            host_prompt = ' %s ' % hostname.split('.')[0]
-
-            powerline.append(host_prompt, FG, BG)
-        else:
             if powerline.args.shell == 'bash':
                 host_prompt = ' \\h '
             elif powerline.args.shell == 'zsh':
--- segments/vcsh.py
+++ segments/vcsh.py
@@ -1,6 +1,6 @@
 import os
 
-def add_vcsh_segment():
+def add_vcsh_segment(powerline):
     vcsh_repo_name = os.getenv('VCSH_REPO_NAME')
     if vcsh_repo_name is None:
         return
@@ -8,5 +8,3 @@ def add_vcsh_segment():
     bg = Color.VCSH_BG
     fg = Color.VCSH_FG
     powerline.append(' %s ' % vcsh_repo_name, fg, bg)
-
-add_vcsh_segment()
